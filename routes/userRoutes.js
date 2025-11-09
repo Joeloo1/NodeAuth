d0b9fc705d2546cd51fd4a626e9e93f129ede17e
Joel <Agboola joel>
2025-11-09 12:40:57 +0100

    feat(auth): add forgot password route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,4 +6,6 @@ const router = express.Router();
 router.route('/signup').post(authController.signup);
 router.route('/login').post(authController.login);
 
+router.post('/forgetPassword', authController.forgetPassword)
+
 module.exports = router;
