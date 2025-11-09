f0d68b95e28930bbbdece9a15ed04821a183a5ad
Joel <agboolajoel23@gmail.com>
2025-11-09 13:22:17 +0100

    feat(auth): add reset password route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,5 +7,6 @@ router.route('/signup').post(authController.signup);
 router.route('/login').post(authController.login);
 
 router.post('/forgetPassword', authController.forgetPassword)
+router.post('/resetPassword/:token', authController.resetPassword)
 
 module.exports = router;
