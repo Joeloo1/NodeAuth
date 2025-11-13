acf13a211917e0dfc1c6a60906acab8ebe8fb085
Joel <agboolajoel123@gmail.com>
2025-11-13 10:58:53 +0100

    refactor error controller

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,18 +1,38 @@
-module.exports = (err, req, res, next) => {
-     err.statusCode = err.statusCode || 500
-     err.status = err.status || 'error'
-
-     if (process.env.NODE_ENV === 'devlopment') {
-        res.status(err.statusCode).json({
+const sendDev = (err, res) => {
+    res.status(err.statusCode).json({
             status: err.status,
             error: err,
             message: err.message,
             stack: err.stack
         })
-     } else if (process.env.NODE_ENV === 'production') {
+}
+
+const sendProd = (err, res) => {
+    // operational, trusted error: send message to client 
+    if (err.isOperational) {
         res.status(err.statusCode).json({
             status: err.status,
             message: err.message,
         })
+        // programming or other unknown error: don't leak to client 
+    } else { 
+        // 1) log the error 
+        console.error('ERROR', err)
+        // send generic message 
+        res.status(500).json({
+            status: 'Fail',
+            message: 'Something went wrong!'
+        })
+    }
+}
+
+module.exports = (err, req, res, next) => {
+     err.statusCode = err.statusCode || 500
+     err.status = err.status || 'error'
+
+     if (process.env.NODE_ENV === 'devlopment') {
+       sendDev(err, res)
+     } else if (process.env.NODE_ENV === 'production') {
+        sendProd(err, res)
      }
 }
