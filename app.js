0a03b668e8d85c389fdab634bbaa184f4f4b9977
joeloo1 <agboolajoel123@gmail.com>
2026-01-02 21:08:08 +0100

    feat(app): add rate limiting and global request logging

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
+const logger = require('./config/logger')
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const AppError = require('./utils/appError');
@@ -9,6 +11,31 @@ const globalErrorHandler = require('./controller/errorController')
 const app = express();
 app.use(morgan('tiny'));
 app.use(express.json());
+
+// Request Limiting from the same IP
+const Limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "To many request from this IP, Please try again in an hour",
+  handler: (req, res) => {
+    logger.warn("Rate limit exceeded", {
+      ip: req.ip,
+      path: req.path,
+    });
+  },
+});
+
+app.use("/api", Limiter);
+
+// Log all Request
+app.use((req, res, next) =>{
+  logger.http("Incoming request...", {
+    method: req.method,
+    path: req.path,
+    ip: req.ip,
+  });
+  next();
+});
 
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/products', productRoutes);
